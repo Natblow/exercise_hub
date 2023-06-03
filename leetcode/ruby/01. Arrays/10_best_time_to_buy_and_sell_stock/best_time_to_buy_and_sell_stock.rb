edb265e468661f42739b8f4a5601fee67a006196
Natblow <natblow@hotmail.fr>
2023-06-03 11:32:48 -0400

    loop solution

--- leetcode/ruby/01. Arrays/10_best_time_to_buy_and_sell_stock/best_time_to_buy_and_sell_stock.rb
+++ leetcode/ruby/01. Arrays/10_best_time_to_buy_and_sell_stock/best_time_to_buy_and_sell_stock.rb
@@ -36,3 +36,19 @@ def max_profit(prices)
     end
     return max
 end
+
+# left = buy, right = sell
+def max_profit(prices)
+    l = 0 # start left at beginning
+    r = 1 # right pointer is one head
+    max_profit = 0 # init max
+    while r != prices.length # until we have searched the entire list
+        if prices[l] < prices[r] # if we find a profit
+            max_profit = [max_profit, prices[r] - prices[l]].max # store it if max profit so far
+        else # no profit, so weve found the max profit for that left pointer value
+            l = r # set it to r 
+        end
+        r += 1 # iterate r regardless
+    end
+    return max_profit
+end
