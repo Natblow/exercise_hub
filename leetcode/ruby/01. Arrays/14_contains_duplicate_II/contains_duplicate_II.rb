bad91a5d39a9350c6827d3de11dfc1dc1695c55a
Natblow <natblow@hotmail.fr>
2023-06-05 18:40:47 -0400

    sliding window solution

--- leetcode/ruby/01. Arrays/14_contains_duplicate_II/contains_duplicate_II.rb
+++ leetcode/ruby/01. Arrays/14_contains_duplicate_II/contains_duplicate_II.rb
@@ -17,3 +17,47 @@ def contains_nearby_duplicate(nums, k)
     end
     false
 end
+
+#Sliding window solution, best runtime & memory usage.
+def contains_nearby_duplicate(nums, k)
+    # When k is 0, we are not looking for duplicates and if there is less than 2 elements in nums then there
+    # can't be any duplicates.
+    if k == 0 || nums.size < 2
+        return false
+    end
+
+    # Initialize set and add first element. 
+    set = {}
+    set[nums[0]] = 1
+    
+    # Using these vars to keep track of our sliding window.
+    window_left_index = 0
+    window_right_index = k
+
+    # We are starting from first index as we added our first element to our set already.
+    nums_index = 1
+    while nums_index < nums.size
+        if set.include? nums[nums_index]
+            return true
+        end
+
+        # Add our item to our set.. I mean hash.
+        set[nums[nums_index]] = 1
+
+        # Increase our index. 
+        nums_index = nums_index + 1
+
+        if window_right_index < nums_index
+            # remove item from set from based on window_left_index
+            set.delete(nums[window_left_index])
+            
+            # update window_right_index
+            window_right_index = window_right_index + 1
+
+            # update window_left_index
+            window_left_index = window_left_index + 1
+        end
+    end
+
+    return false
+end
