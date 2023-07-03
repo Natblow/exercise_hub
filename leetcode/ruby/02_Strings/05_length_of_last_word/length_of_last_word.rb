0bf61a3cc071733c8a1cd5d70f0c12254a50103a
Natblow <natblow@hotmail.fr>
2023-07-02 20:06:43 -0400

    correction & another solution

--- leetcode/ruby/02_Strings/05_length_of_last_word/length_of_last_word.rb
+++ leetcode/ruby/02_Strings/05_length_of_last_word/length_of_last_word.rb
@@ -25,6 +25,7 @@ def length_of_last_word(s)
     end
         
     word_end_pointer - word_start_pointer + 1
+end
 
     
 # Two pointer recursion solution 
@@ -33,4 +34,34 @@ def length_of_last_word(s, word_start_pointer = -1, word_end_pointer = -1)
     return length_of_last_word(s, word_start_pointer - 1, word_end_pointer) unless s[word_start_pointer - 1] == ' ' || s[word_start_pointer - 1].nil?
     
     word_end_pointer - word_start_pointer + 1
+end
+
+
+def length_of_last_word(s)
+    # reverse the string
+    s = s.reverse!
+
+    # initialize counters
+    cursor = 0
+    len = 0
+    count = false
+
+    while cursor < s.length
+      # codepoint of 32 equates to a space
+      if s[cursor].ord == 32 and count == false
+        cursor += 1
+
+      # start counting on non space codepoints
+      elsif s[cursor].ord != 32
+        count = true
+        len += 1
+        cursor += 1
+
+      # stop counting
+      elsif s[cursor].ord == 32 and count == true
+        return len
+      end
+      
+    end
+    return len
 end
